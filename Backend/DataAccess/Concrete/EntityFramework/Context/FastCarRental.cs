7b3f0191eed9953954aab6c9b374a0c4e30af70e
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2024-02-12 15:13:38 +0300

    Create Customer

---
+++ Backend/DataAccess/Concrete/EntityFramework/Context/FastCarRental.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class FastCarRentalContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-HK3Q21E;database=FastCarRentalDB2;integrated security=true");
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
